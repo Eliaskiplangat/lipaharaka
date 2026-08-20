1ab3c8364d13ec318690bdb2b0ef3a2a1502cf82
Eliaskiplangat <eliaskiplangat42@gmail.com>
2026-08-20 09:10:14 +0300

    fix: correct users migration (use Ecto.Migration, def change wrapper)

--- priv/repo/migrations/20260818115254_create_users.exs
+++ priv/repo/migrations/20260818115254_create_users.exs
@@ -1,4 +1,7 @@
 defmodule Lipaharaka.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+    def change do
     create table(:users, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :phone_number, :string, null: false
@@ -13,3 +16,4 @@ defmodule Lipaharaka.Repo.Migrations.CreateUsers do
 
     create unique_index(:users, [:phone_number])
   end
+end
