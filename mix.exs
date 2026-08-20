22d2fb2da441d9da9ea21b87ea65df2599031e3d
Eliaskiplangat <eliaskiplangat42@gmail.com>
2026-08-20 08:46:04 +0300

    fix: add missing bcrypt_elixir and req deps to mix.exs

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule Lipaharaka.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Lipaharaka.Application, []},
@@ -23,13 +20,11 @@ defmodule Lipaharaka.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
+
   defp deps do
     [
       {:phoenix, "~> 1.7.14"},
@@ -39,11 +34,12 @@ defmodule Lipaharaka.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:bcrypt_elixir, "~> 3.1"},
+      {:req, "~> 0.5"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
