9852337baa41b85b2581da7b79e723d7775d4aeb
Eliaskiplangat <eliaskiplangat42@gmail.com>
2026-08-20 09:18:03 +0300

    fix: use Test SMS adapter in test config (was missing, causing OTP tests to fail)

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,6 @@
 import Config
 
-# Configure your database
+
 config :lipaharaka, Lipaharaka.Repo,
   username: "postgres",
   password: "elias",
@@ -10,15 +10,18 @@ config :lipaharaka, Lipaharaka.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
+
 config :lipaharaka, LipaharakaWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "test_secret_key_base_at_least_64_bytes_long_CHANGE_ME_1234567890",
   server: false
 
-# Print only warnings and errors during test
+
+config :lipaharaka, :sms_adapter, Lipaharaka.SMS.Test
+
+
+config :bcrypt_elixir, :log_rounds, 4
+
 config :logger, level: :warning
 
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
